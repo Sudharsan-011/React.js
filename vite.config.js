f76ad248fe8656660893680919f6d63de5c638d0
srsudharsan11 <srsudharsan11@gmail.com>
2024-04-08 20:06:47 +0530

    Update homepage URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: 'React.js',
+  plugins: [react()],
+  build: {
+    minify: true,
+    sourcemap: false,
+    target: 'esnext',
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 });
